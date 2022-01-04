764054bf2caf135c39cee41c5650b91030cb4250
kiranbansode <kiran5120135@gmail.com>
2022-01-04 15:39:21 +0530

    4. Adding an Event Listener

--- 09/shared.js
+++ 09/shared.js
@@ -1,5 +1,10 @@
 let backdrop = document.querySelector(".backdrop");
+let selectPlanButtons = document.querySelectorAll(".plan button");
+let modal = document.querySelector(".modal");
 
-// console.log(backdrop);
-
-backdrop.style.display = "block";
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
