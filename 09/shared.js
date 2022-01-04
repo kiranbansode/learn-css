ff837f56fbbdb2a1713994b6a292144f547574c7
kiranbansode <kiran5120135@gmail.com>
2022-01-04 16:09:20 +0530

    7. Opening and Closing the Hamburger Menu

--- 09/shared.js
+++ 09/shared.js
@@ -1,10 +1,31 @@
 let backdrop = document.querySelector(".backdrop");
-let selectPlanButtons = document.querySelectorAll(".plan button");
 let modal = document.querySelector(".modal");
+let modalNoButton = document.querySelector(".modal__action--negative");
+let selectPlanButtons = document.querySelectorAll(".plan button");
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
 
+// console.dir(backdrop);
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
     modal.style.display = "block";
     backdrop.style.display = "block";
   });
 }
+
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  closeModal();
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
